01bddb66d211525a612faad762b25f017f7e4d50
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-16 11:04:06 +0000

    Real-time data will be collected every minute & only the most recent data will be in the collection

--- scraper/RealTimeBusDataScraping.py
+++ scraper/RealTimeBusDataScraping.py
@@ -1,6 +1,5 @@
 # THIS SCRIPT WILL SCRAPE THE GTFS-R DATA AND INSERT THE DATA IN THE COLLECTION
-# TODO:
-#  (1) REMOVE DUPLICATES BEFORE THEY ARE INSERTED IN THE COLLECTION
+# DATA WILL BE SCRAPED EVERY MINUTE 
 
 
 import urllib.request
@@ -8,7 +7,8 @@ import time
 import certifi
 import ssl
 import json
-import connectionsconfig
+import os
+import configparser
 from pymongo import MongoClient
 
 # error checking when connecting to MongoClient
@@ -17,15 +17,24 @@ try:
 except ImportError:
     raise ImportError('PyMongo is not installed')
 
+# load config file
+print('reading configurations')
+config = configparser.ConfigParser()
+config.read('config/scrapercfg.ini')
+connectionsconfig = config['scraper']
 
 def gtfs_r():
-    # # connecting to MongoDB
-    cluster = MongoClient(connectionsconfig.uri)
+    # # connecting to MongoDB & gtfs_data
+        uri = connectionsconfig['uri']
+        url = connectionsconfig['url']
+        hdr = connectionsconfig['hdr']
+
+    cluster = MongoClient(uri)
     db = cluster["BusData"]  # use a database called "BusData"
-    collection = db["bus"]  # and inside that DB, a collection called "bus"
+    collection = db["real-timeData"]  # and inside that DB, a collection called "real-timeData"
 
     try:
-        req = urllib.request.Request(connectionsconfig.url, headers=connectionsconfig.hdr)
+        req = urllib.request.Request(url, headers=hdr)
 
         req.get_method = lambda: 'GET'
         response = urllib.request.urlopen(req, context=ssl.create_default_context(cafile=certifi.where()))
@@ -36,8 +45,11 @@ def gtfs_r():
         # loading the response into a json file
         json_response = json.loads(gtfs_data.decode('utf-8'))
 
+        # dropping the collection to have only most recent data
+        collection.drop()
+
         # inserting the data in mongodb collection
-        x = collection.insert_one(json_response)
+        collection.insert_one(json_response)
 
     except Exception as e:
         print(e)
@@ -49,7 +61,7 @@ def gtfs_r():
         cluster.close()
 
     # real-time data will be scraped every 10 minutes
-    time.sleep(10 * 60)
+    time.sleep(1 * 60)
 
 
 while True:
