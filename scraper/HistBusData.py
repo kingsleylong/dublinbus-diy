5c224ed8f9603133fb4461632b68c6eb6a251156
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-16 11:15:44 +0000

    1) config updated 2) inserting the historical to mongodb

--- scraper/HistBusData.py
+++ scraper/HistBusData.py
@@ -3,12 +3,16 @@
 import json
 import pandas as pd
 from pymongo import MongoClient
-import connectionsconfig
+import os
+import configparser
+
+# connecting to MongoDB 
+uri = connectionsconfig['uri']
 
 
 try:
     # reading the csv files and creating a pandas df
-    df = pd.read_csv("routes.txt", sep=",", decimal=',')
+    df = pd.read_csv("rt_leavetimes_DB_2018.txt", sep=",", decimal=',')
     df.replace({',', ' '}, {'"', ' '}, regex=True, inplace=True)
 
     # Write to a separate JSON file
@@ -19,14 +23,15 @@ try:
         outfile.write(array_json)
 
     # connecting to mongodb
-    cluster = MongoClient(connectionsconfig.uri)
+    cluster = MongoClient(uri)
     db = cluster["BusData"]  # use a database called "BusData"
-    collection = db["bus"]  # and inside that DB, a collection called "bus"
+    collection = db["historicalData"]  # and inside that DB, a collection called "bus"
 
     # opening the json file created to insert it in the mongodb collection
     with open('json_data.json') as file:
         file_data = json.load(file)
 
+# inserting data into mongodb
     collection.insert_one(file_data)
 except Exception as e:
     print(e)
