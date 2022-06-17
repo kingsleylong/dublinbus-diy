05309f8aa21385094edb8835bb044e42572f6619
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-17 14:12:18 +0000

    Update scraper/HistBusData.py

--- scraper/HistBusData.py
+++ scraper/HistBusData.py
@@ -18,7 +18,7 @@ uri = connectionsconfig['uri']
 def histData():
     try:
         # reading the csv files and creating a pandas df
-        df = pd.read_csv("rt_leavetimes_DB_2018.txt", sep=",", decimal=',')
+        df = pd.read_csv("routes.txt", sep=",", decimal=',')
         df.replace({',', ' '}, {'"', ' '}, regex=True, inplace=True)
 
         # Write to a separate JSON file
