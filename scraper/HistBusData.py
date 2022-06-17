0f4b47667cc8ca11e58981bbfdc7faeb0a566dcb
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-17 14:01:45 +0000

    Update scraper/HistBusData.py

--- scraper/HistBusData.py
+++ scraper/HistBusData.py
@@ -15,35 +15,40 @@ connectionsconfig = config['scraper']
 # connecting to MongoDB 
 uri = connectionsconfig['uri']
 
+def histData():
+    try:
+        # reading the csv files and creating a pandas df
+        df = pd.read_csv("rt_leavetimes_DB_2018.txt", sep=",", decimal=',')
+        df.replace({',', ' '}, {'"', ' '}, regex=True, inplace=True)
 
-try:
-    # reading the csv files and creating a pandas df
-    df = pd.read_csv("rt_leavetimes_DB_2018.txt", sep=",", decimal=',')
-    df.replace({',', ' '}, {'"', ' '}, regex=True, inplace=True)
+        # Write to a separate JSON file
+        array_json = df.to_json(orient='index')
 
-    # Write to a separate JSON file
-    array_json = df.to_json(orient='index')
+        # creating a json file from the data
+        with open('json_data.json', 'w') as outfile:
+            outfile.write(array_json)
 
-    # creating a json file from the data
-    with open('json_data.json', 'w') as outfile:
-        outfile.write(array_json)
+        # connecting to mongodb
+        cluster = MongoClient(uri)
+        db = cluster["BusData"]  # use a database called "BusData"
+        collection = db["historicalData"]  # and inside that DB, a collection called "bus"
 
-    # connecting to mongodb
-    cluster = MongoClient(uri)
-    db = cluster["BusData"]  # use a database called "BusData"
-    collection = db["historicalData"]  # and inside that DB, a collection called "bus"
+        # opening the json file created to insert it in the mongodb collection
+        with open('json_data.json') as file:
+            file_data = json.load(file)
 
-    # opening the json file created to insert it in the mongodb collection
-    with open('json_data.json') as file:
-        file_data = json.load(file)
+    # inserting data into mongodb
+        collection.insert_one(file_data)
+    except Exception as e:
+        print(e)
+    else:
+        print("Data inserted successfully.")
 
-# inserting data into mongodb
-    collection.insert_one(file_data)
-except Exception as e:
-    print(e)
-else:
-    print("Data inserted successfully.")
+    # close the connection
+    finally:
+        cluster.close()
 
-# close the connection
-finally:
-    cluster.close()
+    # exit the script after it run 
+    quit()
+
+histData()
