c73a84a3f4df3a4940fbddf149c21ada0b46fc8d
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-19 12:53:28 +0000

    Update scraper/run.sh

--- scraper/run.sh
+++ scraper/run.sh
@@ -3,6 +3,5 @@
 python3 WeatherCurrent.py &
 python3 WeatherForecast.py &
 python3 RealTimeBusDataScraping.py &
-python3 HistBusData.py 
 
-printf "This is running on 19/06 @11:40"
+printf "This is running on 19/06 @14:00"
