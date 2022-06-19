d47470d910277eaae9d690c759d5383aef24467e
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-19 18:39:37 +0000

    Update scraper/run.sh

--- scraper/run.sh
+++ scraper/run.sh
@@ -5,11 +5,11 @@
 #python3 RealTimeBusDataScraping.py 
 printf "Creating an array with scripts"
 
-scripts = ("WeatherCurrent.py" "WeatherForecast" "RealTimeBusDataScraping")
+scripts=( "WeatherCurrent.py" "WeatherForecast" "RealTimeBusDataScraping" )
 
 printf "Creating for loop to run all scripts"
 for x in scripts
-    python3 $scripts
+    python3 ${scripts[@]}
 done
 
 printf "This is running on 19/06 @19:30"
