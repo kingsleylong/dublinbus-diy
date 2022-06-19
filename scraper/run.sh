cbae2034972973c5dd553ec134bb25f92aec4ea2
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-19 19:30:01 +0000

    Update scraper/run.sh

--- scraper/run.sh
+++ scraper/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#python3 WeatherCurrent.py 
-#python3 WeatherForecast.py 
-#python3 RealTimeBusDataScraping.py 
+python3 WeatherForecast.py
+python3 WeatherCurrent.py  
+python3 RealTimeBusDataScraping.py 
 printf "Creating an array with scripts 19/06 @20"
 
-python WeatherCurrent.py & python WeatherForecast.py & python RealTimeBusDataScraping.py
 
