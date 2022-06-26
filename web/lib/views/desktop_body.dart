027b9fc0259bf4a86349a87049b942c137b7bf40
Kingsley <long.kingsley@gmail.com>
2022-06-27 00:04:10 +0100

    Build a list for the lines in an array

--- web/lib/views/desktop_body.dart
+++ web/lib/views/desktop_body.dart
@@ -10,7 +10,9 @@ class DesktopBody extends StatefulWidget {
 
 class _DesktopBodyState extends State<DesktopBody>
     with TickerProviderStateMixin{
+
   late TabController _tabController;
+  final _lines = <String>["175", "C1", "46A", "52"];
 
   @override
   void initState() {
@@ -22,38 +24,82 @@ class _DesktopBodyState extends State<DesktopBody>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Dublin Bus DIY")),
-      body: Stack(
-        alignment: Alignment.topLeft,
+      body: Row(
+        // alignment: Alignment.topLeft,
         children: [
-          GoogleMapComponent(),
-
-          Container(
-            width: 400,
-            child: Column(
-              children: [
-                ColoredBox(
-                  color: Colors.green,
-                  child: TabBar(
-                    // expand the tab bar out of range and slide the bar when
-                    // clicking tabs at the edges
-                    isScrollable: true,
-                    controller: _tabController,
-                    tabs: const [
-                      Tab(text: "Plan My Journey"),
-                      Tab(text: "Find My Route"),
-                      Tab(text: "Get Me There On-Time"),
-                    ],
-                  ),
-                ),
-              ],
+          // left bar
+          buildLeftBar(),
+          // right information box, use Expanded class to take the rest of space
+          Expanded(
+            child: buildRightInformationBox(),
           )
-
-          //search fields
-
-          //weather info
-          ),
         ],
       )
+    );
+  }
+
+  TabBarView buildRightInformationBox() {
+    return TabBarView(
+      controller: _tabController,
+      children: const <Widget>[
+        Center(
+          child: Text("It's cloudy here"),
+          // child: ,
+        ),
+        Center(
+          child: Text("It's rainy here"),
+        ),
+        Center(
+          child: Text("It's sunny here"),
+        ),
+      ]
+    );
+  }
+
+  SizedBox buildLeftBar() {
+    return SizedBox(
+      width: 350,
+      child: Column(
+        children: [
+          ColoredBox(
+            color: Colors.grey,
+            child: TabBar(
+              // expand the tab bar out of range and slide the bar when clicking
+              // tabs at the edges https://stackoverflow.com/a/60636918
+              isScrollable: true,
+              controller: _tabController,
+              tabs: const [
+                Tab(text: "Plan My Journey"),
+                Tab(text: "Find My Route"),
+                Tab(text: "Get Me There On-Time"),
+              ],
+            ),
+          ),
+
+          //search fields
+          Expanded(
+            // build a list view from data
+            // https://codelabs.developers.google.com/codelabs/first-flutter-app-pt1/#5
+            child: ListView.builder(
+              // specify the length of data, without this an index out of range error will be
+              // thrown. https://stackoverflow.com/a/58850610
+              itemCount: _lines.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    _lines[index],
+                    // strutStyle: ,
+                  ),
+                );
+              }
+            ),
+          )
+
+          //weather info
+          // TODO
+
+        ],
+      ),
     );
   }
 }
