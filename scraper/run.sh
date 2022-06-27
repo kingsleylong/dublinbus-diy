bccfbdb8911e6107aa769533ba6d92c0991601d0
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-06-27 07:32:55 +0000

    Update run.sh
    Former-commit-id: dfc9570313ee496d44393e8ecbfd903fec0a57f7

--- scraper/run.sh
+++ scraper/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#nohup python3 -u RealTimeBusDataScraping.py &
-#nohup python3 -u WeatherCurrent.py &
- python3 -u StoreGTFSR.py 
-#python3 -u WeatherForecast.py
+nohup python3 -u RealTimeBusDataScraping.py &
+nohup python3 -u WeatherCurrent.py &
+nohup python3 -u StoreGTFSR.py &
+python3 -u WeatherForecast.py
