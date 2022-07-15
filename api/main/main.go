615d7e9445134cc4c3bf9682072a9de85b052acc
Ian Foster <fosterian95@gmail.com>
2022-07-15 16:02:28 +0100

    Updated api function handlers
    Former-commit-id: 5f811189ec07b504f3c9f20e12aefeb25890a998

--- api/main/main.go
+++ api/main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"example.com/api/databaseQueries"
-	"example.com/api/geocoding"
 	"github.com/gin-gonic/gin"
 	"log"
 )
@@ -15,13 +14,11 @@ func main() {
 
 	// Bus Stop specific queries
 	router.GET("/databases", databaseQueries.GetDatabases)
-	router.GET("/findStopByName/:stopName", databaseQueries.GetStopByName)
+	router.GET("/stopsList/:stopSearch", databaseQueries.GetStopsList)
 
 	// Bus Route queries
 	router.GET("/matchingRoute/:originStopNum/:destStopNum", databaseQueries.FindMatchingRoute)
 	router.GET("/matchingRouteTest/:originStopNum", databaseQueries.FindMatchingRouteDemo)
-
-	router.GET("/geocoding/:address", geocoding.GetCoordinates)
 
 	err := router.Run("0.0.0.0:8080")
 	if err != nil {
