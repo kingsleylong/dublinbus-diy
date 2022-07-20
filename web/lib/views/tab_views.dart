dc98c1ca93008356992530415836448c2baaa38c
Kingsley <long.kingsley@gmail.com>
2022-07-20 12:19:12 +0100

    support nearby stops in the dropdowns
    Former-commit-id: d4751eeb1d0d311fb852ee9cd10afa36df57caca

--- web/lib/views/tab_views.dart
+++ web/lib/views/tab_views.dart
@@ -204,6 +204,8 @@ class _PlanMyJourneyTabViewState extends State<PlanMyJourneyTabView> {
   }
 
   Future<List<BusStop>> fetchFutureBusStopsByName(String filter) async {
+    List<BusStop> busStopList = [];
+
     String url = '$apiHost/api/stop/findByAddress';
     // final paramsStr = (filter == '') ? '' : '&filter=$filter';
     final paramsStr = (filter == '') ? '' : '/$filter';
@@ -217,17 +219,21 @@ class _PlanMyJourneyTabViewState extends State<PlanMyJourneyTabView> {
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response, then parse the JSON.
       final Map<String, dynamic> busStopsJson = jsonDecode(response.body);
-      final List matchedStopsJson = busStopsJson['matched'];
-      final List nearbyStopsJson = busStopsJson['nearby'];
+      final List? matchedStopsJson = busStopsJson['matched'];
+      final List? nearbyStopsJson = busStopsJson['nearby'];
 
-      print("Bus stops size: matched ${matchedStopsJson.length}, suggested ${nearbyStopsJson.length}");
-      List<BusStop> matchedStopList = List.generate(matchedStopsJson.length, (index) =>
+      List<BusStop> matchedStopList;
+      List<BusStop> nearbyStopList;
+      if (matchedStopsJson != null) {
+        matchedStopList = List.generate(matchedStopsJson.length, (index) =>
           BusStop.fromJson(matchedStopsJson[index], BusStopType.matched));
-      List<BusStop> nearbyStopList = List.generate(nearbyStopsJson.length, (index) =>
-          BusStop.fromJson(matchedStopsJson[index], BusStopType.nearby));
-      List<BusStop> busStopList = [];
-      busStopList.addAll(matchedStopList);
-      busStopList.addAll(nearbyStopList);
+        busStopList.addAll(matchedStopList);
+      }
+      if (nearbyStopsJson != null) {
+        nearbyStopList = List.generate(nearbyStopsJson.length, (index) =>
+          BusStop.fromJson(nearbyStopsJson[index], BusStopType.nearby));
+        busStopList.addAll(nearbyStopList);
+      }
       return busStopList;
     } else {
       // If the server did not return a 200 OK response, then throw an exception.
