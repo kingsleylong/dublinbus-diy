86454a930450fc3bed944e2e30f91a756bb7f6a3
Kingsley <long.kingsley@gmail.com>
2022-07-26 09:51:15 +0100

    remove unnecessary fields
    Former-commit-id: b16fe789a30a1a36ef34b71a8b96373a31e991ed

--- web/lib/models/shape.dart
+++ web/lib/models/shape.dart
@@ -1,17 +1,13 @@
 class Shape {
   double latitude;
   double longitude;
-  int sequence;
-  String distanceTraveled;
 
-  Shape(this.latitude, this.longitude, this.sequence, this.distanceTraveled);
+  Shape(this.latitude, this.longitude);
 
   factory Shape.fromJson(Map<String, dynamic> json) {
     return Shape(
       json['shape_pt_lat'],
       json['shape_pt_lon'],
-      json['shape_pt_sequence'],
-      json['shape_dist_traveled'],
     );
   }
 }
