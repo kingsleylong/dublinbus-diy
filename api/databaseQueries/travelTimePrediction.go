dce1b8401be881caa79694a7a4d51e9671616969
Ian Foster <fosterian95@gmail.com>
2022-07-28 10:58:12 +0100

    Adding main function for obtaining travel time predictions
    Former-commit-id: fd2e883c6a5c4bae40f5232cfbf0333601b774f2

--- api/databaseQueries/travelTimePrediction.go
+++ api/databaseQueries/travelTimePrediction.go
@@ -11,7 +11,8 @@ import (
 
 func GetTravelTimePredictionTest(c *gin.Context) {
 
-	resp, err := http.Get("http://localhost:5000/prediction/102/1/3/12/4/64800/2022-07-27 14:00:00")
+	resp, err := http.
+		Get("http://ec2-34-239-115-43.compute-1.amazonaws.com/prediction/102/1/3/12/4/64800/2022-07-28 14:00:00")
 	if err != nil {
 		log.Print(err)
 	}
@@ -35,6 +36,29 @@ func GetTravelTimePredictionTest(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, result)
 }
 
-//func GetTravelTimePrediction() []float64 {
-//
-//}
+func GetTravelTimePrediction(routeNum string,
+	date string,
+	direction string) TravelTimePrediction {
+
+	dayOfWeek := DayOfTheWeek(date)
+	hour, month := DateExtraction(date)
+	departureTime := SecondsExtraction(date)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	bodyString := string(body)
+	bodyStringAdjusted := strings.Replace(bodyString, "[", "", 1)
+	bodyStringAdjusted = strings.Replace(bodyStringAdjusted, "]\n", "", 1)
+	bodyStrings := strings.Split(bodyStringAdjusted, ",")
+
+	var travelTime TravelTimePrediction
+
+	travelTime.TransitTime, _ = strconv.ParseFloat(bodyStrings[0], 64)
+	travelTime.TransitTimePlusMAE, _ = strconv.ParseFloat(bodyStrings[1], 64)
+	travelTime.TransitTimeMinusMAE, _ = strconv.ParseFloat(bodyStrings[2], 64)
+
+	return travelTime
+}
