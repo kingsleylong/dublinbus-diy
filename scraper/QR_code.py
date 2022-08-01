b64fabe7deff275f540bd5cfbfacabbdb87eb794
diana lenghel <diana.lenghel@ucdconnect.ie>
2022-08-01 11:34:16 +0000

    function to create an image for a QR code
    Former-commit-id: 9c0b27c750e3d3788229812313395e89cf0bd523

--- scraper/QR_code.py
+++ scraper/QR_code.py
@@ -1,1 +1,22 @@
+import uuid
+import qrcode
+
+def QrCode(input_data):
+        #Creating an instance of qrcode
+        qr = qrcode.QRCode(
+            version=1,
+            box_size=10,
+            border=5)
+        qr.add_data(input_data)
+        qr.make(fit=True)
+        img = qr.make_image(fill='black', back_color='white')
+        filename = img.save('qr_code_' + str(uuid.uuid4()) + '.png')
+        return filename
+
+# I put the link to our desktop app, and 2 links to App and Play store 
+# Todo: change links when the app is up on the store
+qr_code_list = ["http://ipa-003.ucd.ie/#/","https://apps.apple.com/ie/app/planny-day-planner/id1515324201", "https://play.google.com/store/apps/details?id=com.thomasyoung.dadish3"]
+
+for line in qr_code_list:
+    QrCode({line})
 
