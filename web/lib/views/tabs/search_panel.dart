a38ac26614a48f17ee343937ba5ac721396b4333
Kingsley <long.kingsley@gmail.com>
2022-08-06 12:24:51 +0100

    use geolocator package to get the platform specific location (the location package didn't work on the safari web browser so I have to try another one)
    Former-commit-id: a21fc0a6b1ec0e011598fc4f5770ac2d363690aa

--- web/lib/views/tabs/search_panel.dart
+++ web/lib/views/tabs/search_panel.dart
@@ -1,8 +1,8 @@
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:intl/intl.dart';
-import 'package:location/location.dart';
 import 'package:provider/provider.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
@@ -27,44 +27,63 @@ class SearchForm extends StatefulWidget {
 class _SearchFormState extends State<SearchForm> {
   late Future<List<BusRoute>> futureBusRoutes;
 
-  // get user location https://docs.page/Lyokone/flutterlocation
-  Location location = Location();
-  late bool _serviceEnabled;
-  late PermissionStatus _permissionGranted;
-  late LocationData _locationData;
-
   @override
   void initState() {
     super.initState();
     checkState();
   }
 
-  checkState() async {
-    try {
-      _serviceEnabled = await location.serviceEnabled();
-      if (!_serviceEnabled) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled) {
-          return;
-        }
-      }
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (_permissionGranted != PermissionStatus.granted) {
-          return;
-        }
-      }
-      _locationData = await location.getLocation();
-      print('${_locationData.latitude},${_locationData.longitude},${_locationData.accuracy}, '
-          '${_locationData.headingAccuracy},${_locationData.provider}');
-    } catch (e) {
-      print('error getting location: ${e.toString()}');
-      rethrow;
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+
+      // TODO: direct user to the settings page
+      // await Geolocator.openAppSettings();
+      // await Geolocator.openLocationSettings();
+      // add an alert dialog to direct user to the settings page
+
+      return Future.error('Location services are disabled.');
     }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 
-  // late List<Item> items;
+  checkState() async {
+    var determinePosition = await _determinePosition();
+    print(determinePosition);
+  }
 
   @override
   Widget build(BuildContext context) {
