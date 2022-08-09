92e94a01601b0eb9d15f489428bfb23ce63354c6
Diana Lenghel <diana.lenghel@ucdconnect.ie>
2022-08-09 11:19:24 +0100

    favourite route feature
    Former-commit-id: 4b68898b835ed43d718c89aa05170023d3e39850

---
+++ web/lib/views/fav_page.dart
@@ -0,0 +1,173 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:http/http.dart';
+import 'package:provider/provider.dart';
+import '../../models/app_model.dart';
+import '../../models/bus_route.dart';
+import '../../models/bus_route.dart';
+import '../../models/map_polylines.dart';
+import '../../models/search_form.dart';
+import '../views/googlemap_mobile.dart';
+import '../views/tabs/fares_table.dart';
+import 'package:localstorage/localstorage.dart';
+
+import 'tabs/route_options.dart';
+
+class RouteFavOptions extends StatefulWidget {
+  const RouteFavOptions({Key? key}) : super(key: key);
+
+  @override
+  State<RouteFavOptions> createState() => FavoritePage();
+}
+
+// create the fav page
+class FavoritePage extends State<RouteFavOptions> {
+  final RouteList list = new RouteList();
+  final LocalStorage storage = new LocalStorage('fav_routes');
+  bool initialized = false;
+
+  _toggleItem(RouteItem route) {
+    setState(() {
+      route.favourite = !route.favourite;
+      _saveToStorage();
+    });
+  }
+
+  _saveToStorage() {
+    storage.setItem('favourite', list.toJSONEncodable());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Favourites"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Row(
+          children: [
+            // Column(
+            //   Container(
+            // padding: EdgeInsets.all(100),
+            // constraints: BoxConstraints.expand(),
+
+            // child: FutureBuilder(
+            //   future: storage.ready,
+            //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+            //     if (snapshot.data == null) {
+            //       return Center(
+            //         child: CircularProgressIndicator(),
+            //       );
+            //     }
+
+            // children:
+            // [
+            // if (!initialized) {
+            //   var items = storage.getItem('favourite');
+
+            //   if (items != null) {
+            //     list.favoriteRouteList = List<RouteItem>.from(
+            //       (items as List).map(
+            //         (item) => RouteItem(
+            //           route: item['Route'],
+            //           favourite: item['favourite_route'],
+            //         ),
+            //       ),
+            //     );
+            //   }
+
+            //   initialized = true;
+            // }
+            // List<Widget> widgets = list.favoriteRouteList.map((item) {
+            //   return CheckboxListTile(
+            //     value: item.favourite,
+            //     title: Text(item.route),
+            //     selected: item.favourite,
+            //     onChanged: (_) {
+            //       _toggleItem(item);
+            //     },
+            //   );
+            // }).toList();
+
+            // return Text("No fav route");
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.only(top: 50),
+                children: [
+                  favoriteRouteList.isEmpty
+                      ? const Center(
+                          child: Text(
+                            'There are no favorites routes!',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        )
+                      : ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: favoriteRouteList.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(20.0),
+                                      child: Text(
+                                        // storage.ready;
+                                        // storage.getItem('1'),
+                                        favoriteRouteList[index],
+                                        style: const TextStyle(fontSize: 19.0),
+                                      ),
+                                    ),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      storage.ready;
+                                      print(
+                                          "deleting the route from favourite");
+                                      storage.deleteItem('1');
+                                      // this deletes from the array - it doesn't save to localstorage
+                                      // setState(() {
+                                      //   favoriteRouteList
+                                      //       .remove(favoriteRouteList[index]);
+                                      // });
+                                    },
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                        Colors.white,
+                                      ),
+                                    ),
+                                    child: const Icon(
+                                      Icons.remove_circle,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                ],
+              ),
+            ),
+          ],
+        ));
+    //     ],
+    //   ),
+    // );
+  }
+}
+// ];
+// },
+// );
+// ),
+// );
+// }
+// }
--- web/lib/views/tabs/route_options.dart
+++ web/lib/views/tabs/route_options.dart
@@ -8,6 +8,36 @@ import '../../models/map_polylines.dart';
 import '../../models/search_form.dart';
 import '../googlemap_mobile.dart';
 import 'fares_table.dart';
+import 'package:localstorage/localstorage.dart';
+
+List<String> favoriteRouteList = [];
+
+// Map<String, dynamic> favoriteRouteList = {};
+
+class RouteItem {
+  String route;
+  bool favourite;
+
+  RouteItem({required this.route, required this.favourite});
+
+  toJSONEncodable() {
+    Map<String, dynamic> m = new Map();
+
+    m['route_num'] = route;
+    m['favourite'] = favourite;
+    return m;
+  }
+}
+
+class RouteList {
+  List<RouteItem> favoriteRouteList = [];
+
+  toJSONEncodable() {
+    return favoriteRouteList.map((item) {
+      return item.toJSONEncodable();
+    }).toList();
+  }
+}
 
 class RouteOptions extends StatefulWidget {
   const RouteOptions({Key? key}) : super(key: key);
@@ -17,22 +47,120 @@ class RouteOptions extends StatefulWidget {
 }
 
 class _RouteOptionsState extends State<RouteOptions> {
+  // Box box = Hive.box(favRoutes);
+  // bool _isFavorite = false;
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _isFavorite = box.get(0) ?? false;
+  // }
+
+  // trying to use the localstorage package
+  // void initState() {
+  //   super.initState();
+
+  //   storage.ready.then((_) => printStorage());
+  //   //storage.setItem("level", 0);
+  //   printStorage();
+  // }
+
+  // void printStorage() {
+  //   print("level stored: " + storage.getItem("level").toString());
+  // }
+  final RouteList list = new RouteList();
+  final LocalStorage storage = new LocalStorage('fav_routes');
+  bool initialized = false;
+
+  _toggleItem(RouteItem route) {
+    setState(() {
+      route.favourite = !route.favourite;
+      _saveToStorage();
+    });
+  }
+
+  _addItem(String route) {
+    setState(() {
+      final item = new RouteItem(route: route, favourite: false);
+      list.favoriteRouteList.add(item);
+      _saveToStorage();
+    });
+  }
+
+  _deleteItem(String route) {
+    setState(() {
+      final item = new RouteItem(route: route, favourite: false);
+      list.favoriteRouteList.remove(item);
+      _deleteFromStorage();
+    });
+  }
+
+  _saveToStorage() {
+    storage.setItem('favourite', list.toJSONEncodable());
+  }
+
+  _deleteFromStorage() {
+    storage.deleteItem('favourite');
+  }
+
+  _clearStorage() async {
+    await storage.clear();
+
+    setState(() {
+      list.favoriteRouteList = storage.getItem('favourite') ?? [];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<SearchFormModel>(
-      builder: (context, model, child) =>
-          Provider.of<SearchFormModel>(context).visibilityRouteOptions
-              ? SingleChildScrollView(child: _buildRouteOptionPanels(model.busRouteItems))
-              : const Center(child: CircularProgressIndicator()),
-    );
+    // if (box == null) {
+    //   return CircularProgressIndicator();
+    // } else
+    //   Box box = Hive.box(favRoutes);
+    if (Provider.of<SearchFormModel>(context).visibilityRouteOptions) {
+      return Consumer<SearchFormModel>(
+        builder: (context, model, child) => SingleChildScrollView(
+          child: _buildRouteOptionPanels(model.busRouteItems),
+        ),
+      );
+      //   const Expanded(
+      //     child: Padding(
+      //       padding: EdgeInsets.all(8),
+      //       child: RouteOptions(),
+      //     ),
+      //   ),
+      // ConstrainedBox(
+      //   constraints: const BoxConstraints(
+      //     minHeight: 2.0,
+      //   ),
+      // ),
+
+      // return FutureBuilder<List<BusRoute>>(
+      //   future: Provider.of<SearchFormModel>(context).busRoutes,
+      //   builder: (context, snapshot) {
+      //     if (snapshot.hasData) {
+      //       return SingleChildScrollView(
+      //         child: _buildRouteOptionPanels(snapshot.data!),
+      //       );
+      //     } else if (snapshot.hasError) {
+      //       return Text('${snapshot.error}');
+      //     }
+      //     // By default, show a loading spinner.
+      //     return const Center(
+      //       child: CircularProgressIndicator(),
+      //     );
+      //   },
+      // );
+    } else {
+      return Container();
+    }
   }
 
   _buildRouteOptionPanels(List<Item>? items) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
     if (items == null || items.isEmpty) {
       return const Center(child: Text('No routes found.'));
     }
     print("items size: ${items.length}, first ele: ${items[0].toString()}");
+
     // Use ExpansionPanel to display the route options for easy use.
     // https://api.flutter.dev/flutter/material/ExpansionPanel-class.html
     return ExpansionPanelList(
@@ -49,22 +177,21 @@ class _RouteOptionsState extends State<RouteOptions> {
       },
       children: items.map<ExpansionPanel>((Item item) {
         var busRoute = item.busRoute;
-        var fares = busRoute.fares;
-        var travelTimes = busRoute.travelTimes;
+        // print(busRoute.routeNumber);
+        var fares = item.busRoute.fares;
         return ExpansionPanel(
           canTapOnHeader: true,
           headerBuilder: (BuildContext context, bool isExpanded) {
             return ListTile(
+              // TODO: here need to add the fav icon button and create the list
+              // create the list for the fav route
               title: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      const Padding(
-                        padding: EdgeInsets.only(right: 4),
-                        child: Icon(Icons.directions_bus),
-                      ),
+                      const Icon(Icons.directions_bus),
                       Text(
                         busRoute.routeNumber,
                         textAlign: TextAlign.center,
@@ -73,37 +200,122 @@ class _RouteOptionsState extends State<RouteOptions> {
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                          padding: const EdgeInsets.only(right: 4),
-                          child: travelTimes.source == TravelTimeSources.static
-                          // tooltip: https://api.flutter.dev/flutter/material/Tooltip-class.html
-                              // travel time from static table
-                              ? const Tooltip(
-                                  message: 'Travel time from static time table',
-                                  child: Icon(Icons.timer_outlined),
-                                )
-                              // travel time from prediction
-                              : Tooltip(
-                                  message: 'Predicted travel time: ${travelTimes.transitTimeMin}'
-                                      ' - ${travelTimes.transitTimeMax} min',
-                                  child: const Icon(Icons.update),
-                                )),
-                      // sized box sets a fixed width of the text and align them vertically
-                      SizedBox(
-                        width: 60,
-                        child: Text(
-                          // '${busRoute.travelTimes?.transitTimeMin} - ${busRoute.travelTimes?.transitTimeMax} min',
-                          '${travelTimes.transitTime} min',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
+                      ElevatedButton(
+                        onPressed: () {
+                          // this should save the route to localstorage
+                          storage.ready;
+                          print("setting the route as favourite");
+                          storage.setItem('1', busRoute.routeNumber);
+//                        this saves to the array - it doesn't save to localstorage
+                          // setState(() {
+                          //   favoriteRouteList.add(busRoute.routeNumber);
+                          //   print(favoriteRouteList);
+                          // });
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.white,
                           ),
                         ),
+                        child: const Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ),
+                      // elevated button to save fav route
+                      // ElevatedButton(
+
+                      //   onPressed: _addItem(busRoute.routeNumber),
+                      //   style: ButtonStyle(
+                      //     backgroundColor: MaterialStateProperty.all<Color>(
+                      //       Colors.white,
+                      //     ),
+                      //   ),
+                      //   child: const Icon(
+                      //     Icons.favorite,
+                      //     color: Colors.red,
+                      //   ),
+                      // ),
+                      // {
+                      //   // print(favoriteRouteList);
+                      //   setState(() {
+                      //     // favoriteRouteList.add(busRoute.routeNumber);
+                      //     if (!initialized) {
+                      //       var items = storage.getItem('favourite');
+
+                      //       if (items != null) {
+                      //         list.favoriteRouteList = List<RouteItem>.from(
+                      //           (items as List).map(
+                      //             (item) => RouteItem(
+                      //               route: item['Route'],
+                      //               favourite: item['favourite_route'],
+                      //             ),
+                      //           ),
+                      //         );
+                      //       }
+
+                      //       initialized = true;
+                      //     }
+                      //     List<Widget> widgets =
+                      //         list.favoriteRouteList.map((item) {
+                      //       return CheckboxListTile(
+                      //         value: item.favourite,
+                      //         title: Text(item.route),
+                      //         selected: item.favourite,
+                      //         onChanged: (_) {
+                      //           _toggleItem(item);
+                      //         },
+                      //       );
+                      //     }).toList();
+
+                      //     // print(favoriteRouteList);
+                      //   });
+                      // },
+
+                      // icon button to save fav
+                      // IconButton(
+                      //   icon: Icon(Icons.favorite_border),
+                      //   onPressed: printStorage,
+                      //   color: Colors.red,)
+                      //  () {
+                      //   // setState(() {
+                      //   //   _isFavorite = !_isFavorite;
+                      //   // });
+
+                      //   // box.put(0, _isFavorite);
+                      // },
+
+                      // ElevatedButton(
+                      //   onPressed: () {},
+                      //   child: Icon(Icons.favorite_border),
+                      // )
+                      // FavoriteButton(
+                      //   isFavorite: false,
+                      //   iconSize: 10,
+                      //   valueChanged: (_isFavorite) {
+                      //     print('Is Favorite : $_isFavorite');
+                      //   },
+                      // ),
+                      // ],
+                      // ),
+                      // Text(
+                      //   busRoute.stops[0].stopName,
+                      //   style: const TextStyle(
+                      //     fontSize: 16,
+                      //     fontWeight: FontWeight.bold,
+                      //   ),
+                      // ),
+                      Row(
+                        children: [
+                          const Icon(Icons.timer_outlined),
+                          Text(
+                            '${busRoute.travelTimes?.transitTimeMin} - ${busRoute.travelTimes?.transitTimeMax} min',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
@@ -112,35 +324,26 @@ class _RouteOptionsState extends State<RouteOptions> {
             );
           },
           body: ListTile(
-            onTap: () {
-              setState(() {
-                // always collapse the panel on tapping the body
-                item.isExpanded = false;
-              });
-              if (Provider.of<AppModel>(context, listen: false).screenSize == ScreenType.mobile) {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => const GoogleMapMobileComponent()));
-              }
-            },
             title: Column(
               children: [
                 Text(item.expandedValue),
-                FaresTable(fares: fares),
+                const Text('Fares:'),
+                // Use Wrap to arrange the children widgets horizontally
+                // https://stackoverflow.com/a/50096780
+                Wrap(spacing: 10, children: [
+                  // use the Null-coalescing operators to provide an alternative value
+                  // when the expression evaluates to null
+                  // https://dart.dev/codelabs/null-safety#exercise-null-coalescing-operators
+                  Text('Adult Leap: €${fares.adultLeap ?? '-'}'),
+                  Text('Adult Cash: €${fares.adultCash ?? '-'}'),
+                  Text('Child Cash: €${fares.childCash ?? '-'}'),
+                  Text('Child Leap: €${fares.childLeap ?? '-'}'),
+                  Text('Student Leap: €${fares.studentLeap ?? '-'}'),
+                ]),
               ],
             ),
-            subtitle: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Touch to see the route on map',
-                    style: textTheme.bodyLarge,
-                  ),
-                ),
-                Text(item.expandedDetailsValue),
-              ],
+            subtitle: Center(
+              child: Text(item.expandedDetailsValue),
             ),
           ),
           isExpanded: item.isExpanded,
@@ -148,4 +351,116 @@ class _RouteOptionsState extends State<RouteOptions> {
       }).toList(),
     );
   }
+
+  // void addItemsToLocalStorage() {
+  //   final item = json.encode(RouteItem(route: , favourite: false));
+  //   storage.setItem('info', item);
+  // }
 }
+
+// class RouteFavOptions extends StatefulWidget {
+//   const RouteFavOptions({Key? key}) : super(key: key);
+
+//   @override
+//   State<RouteFavOptions> createState() => FavoritePage();
+// }
+
+// // create the fav page
+// class FavoritePage extends State<RouteFavOptions> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Favourites"),
+//           centerTitle: true,
+//           backgroundColor: Colors.blue,
+//         ),
+//         drawer: new Drawer(),
+//         body: TabBarView(
+//           children: [
+//             favoriteRouteList.isEmpty
+//                 ? const Center(
+//                     child: Text(
+//                       'There are no favorites yet!',
+//                       style: TextStyle(color: Colors.black),
+//                     ),
+//                   )
+//                 : ListView.builder(
+//                     itemCount: favoriteRouteList.length,
+//                     itemBuilder: (context, index) {
+//                       return Card(
+//                         child: Row(
+//                           children: [
+//                             Expanded(
+//                               child: Padding(
+//                                 padding: const EdgeInsets.all(20.0),
+//                                 child: Text(
+//                                   favoriteRouteList[index],
+//                                   style: const TextStyle(fontSize: 19.0),
+//                                 ),
+//                               ),
+//                             ),
+//                             ElevatedButton(
+//                               onPressed: () {
+//                                 setState(() {
+//                                   favoriteRouteList
+//                                       .remove(favoriteRouteList[index]);
+//                                 });
+//                               },
+//                               style: ButtonStyle(
+//                                 backgroundColor:
+//                                     MaterialStateProperty.all<Color>(
+//                                   Colors.deepPurple,
+//                                 ),
+//                               ),
+//                               child: const Icon(
+//                                 Icons.remove,
+//                                 color: Colors.white,
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       );
+//                     },
+//                   ),
+//           ],
+//         ));
+//   }
+// }
+
+// this is for hivebox ,
+// drawer: new Drawer(),
+//       body: ValueListenableBuilder(
+//         valueListenable: Hive.box(favRoutes).listenable(),
+//         builder: (context, box, child) {
+//           List posts = List.from(box.values);
+//           return ListView(
+//             padding: const EdgeInsets.all(16.0),
+//             children: [
+//               Text("This is fav page"),
+//               ...posts.map(
+//                 (p) => ListTile(
+//                   title: Text(p['title']),
+//                   trailing: IconButton(
+//                     icon: Icon(
+//                       Hive.box(favRoutes).containsKey(p['id'])
+//                           ? Icons.favorite
+//                           : Icons.favorite_border,
+//                     ),
+//                     onPressed: () {
+//                       if (Hive.box(favRoutes).containsKey(p['id'])) {
+//                         Hive.box(favRoutes).delete(p['id']);
+//                       } else {
+//                         Hive.box(favRoutes).put(p['id'], p);
+//                       }
+//                     },
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
