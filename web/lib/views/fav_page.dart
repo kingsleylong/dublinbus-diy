b3d17ac7c4d2a26f5266a222c6ce03554761d177
Kingsley <long.kingsley@gmail.com>
2022-08-11 22:35:03 +0100

    add favorite button to the favorite route list items
    Former-commit-id: 5c8e9f94ef381f002926992507755bddcde26c9e

--- web/lib/views/fav_page.dart
+++ web/lib/views/fav_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../models/search_form.dart';
+import '../models/bus_route.dart';
 import '../models/map_polylines.dart';
 import '../views/googlemap_mobile.dart';
 
@@ -43,21 +44,39 @@ class FavoritePage extends State<RouteFavOptions> {
             shrinkWrap: true,
             itemCount: favoriteRoutes.length,
             itemBuilder: (context, index) {
+              var routeItem = favoriteRoutes[index].route;
               return InkWell(
                   child: Card(
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Text(
-                              favoriteRoutes[index].route.routeNumber,
-                              style: const TextStyle(fontSize: 19.0),
+                    child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(right: 4),
+                            child: Icon(Icons.directions_bus),
+                          ),
+                          Container(
+                            color: Colors.amberAccent,
+                            child: Padding(
+                              padding: const EdgeInsets.all(3.0),
+                              child: Text(
+                                routeItem.routeNumber,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
+                          const Padding(
+                            padding: EdgeInsets.only(right: 7),
+                          ),
+                          Text(routeItem.travelTimes.scheduledDepartureTime),
+                        ],
+                      ),
+                      buildFavoriteButton(routeItem, searchFormModel)
+                    ]),
                   ),
                   onTap: () {
                     print("Click to show route on map");
@@ -66,6 +85,23 @@ class FavoritePage extends State<RouteFavOptions> {
                   });
             });
       },
+    );
+  }
+
+  buildFavoriteButton(BusRoute busRoute, SearchFormModel searchFormModel) {
+    var favoriteRoute = searchFormModel.favoriteRoutes[busRoute.routeNumber];
+    bool isFavorite = false;
+    if (favoriteRoute != null) {
+      isFavorite = favoriteRoute.favourite;
+    }
+    return IconButton(
+      onPressed: () {
+        searchFormModel.toggleFavorite(busRoute);
+      },
+      icon: Icon(
+        Icons.favorite,
+        color: isFavorite ? Colors.red : Colors.grey,
+      ),
     );
   }
 }
